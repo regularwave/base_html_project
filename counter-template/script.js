edc8eca3b119728c96993e45e4024500f0471e86
regularwave <regularwave@gmail.com>
2022-10-08 20:29:09 -0400

    added page counter

---
+++ counter-template/script.js
@@ -0,0 +1,13 @@
+const visCount = document.getElementById('visCount');
+updateSiteCounter();
+
+function updateSiteCounter() {
+    fetch('https://api.countapi.xyz/hit/::UNIQUEKEY::')
+        .then(response => response.json())
+        .then(response => {
+            visCount.innerHTML = response.value;
+        })
+}
+
+// HTML:
+// Visits: <span id="visCount"></span> 
